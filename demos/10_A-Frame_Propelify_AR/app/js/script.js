bbdc632c437b8611c1968e7e3281fef0a6fac371
rdub80 <rdub80@gmail.com>
2018-05-09 18:10:54 -0400

    propelify first release

--- demos/10_A-Frame_Propelify_AR/app/js/script.js
+++ demos/10_A-Frame_Propelify_AR/app/js/script.js
@@ -41,75 +41,35 @@ AFRAME.registerComponent('shadow-material', {
     }
 });
 
-AFRAME.registerComponent('wall-shadow', {
-  schema: {
-    // Add properties.
-  },
+AFRAME.registerComponent("hoverable", {
+    init: function () {
+        var el = this.el,
+            logo1 = document.getElementById('logo1'),
+            logo2 = document.getElementById('logo2'),
+            bulb = document.getElementById('bulb');
+        el.addEventListener("mouseenter", function () {
+            this.setAttribute("scale", " 0.35 0.35 0.35");
+            bulb.setAttribute("position", "0 0.5 0");
+            bulb.setAttribute("radius", "0.15");
+            logo1.setAttribute("radius", "0.5");
+            logo2.setAttribute("radius", "0.5");
+            logo1.setAttribute("height", "0.5");
+            logo2.setAttribute("height", "0.5");
+            bulb.components.light.light.intensity = 0.75;
+        });
+        el.addEventListener("mouseleave", function () {
+            this.setAttribute("scale", "0.25 0.25 0.25");
+            bulb.setAttribute("position", "0 0.25 0");
+            bulb.setAttribute("radius", "0.085");
+            logo1.setAttribute("radius", "0.05");
+            logo2.setAttribute("radius", "0.05");
+            logo1.setAttribute("height", "0.05");
+            logo2.setAttribute("height", "0.05");
+            bulb.components.light.light.intensity = 0.25;
+        });
 
-  init: function () {
-
-
-    var top = document.createElement("a-entity");
-    top.setAttribute('geometry', 'primitive: plane; width:1; height:1;');
-    top.setAttribute('shadow', 'receive: true');
-    top.setAttribute('material', 'shader:flat; opacity:0.3; color: white');
-    top.setAttribute('position', '0 1 0.01');
-    // top.el.getOrCreateObject3D('mesh').material = new THREE.ShadowMaterial({
-    //   // ...
-    // });
-    // top.material.opacity = 0.3;
-    this.el.appendChild(top);
-
-    var left = document.createElement("a-entity");
-    left.setAttribute('geometry', 'primitive: plane; width:1; height:1;');
-    left.setAttribute('shadow', 'receive: true');
-    left.setAttribute('material', 'shader:flat; opacity:0.3; color: white');
-    left.setAttribute('position', '-1 0 0.01');
-    // left.el.getOrCreateObject3D('mesh').material = new THREE.ShadowMaterial({
-    //   // ...
-    // });
-    // left.material.opacity = 0.3;
-    this.el.appendChild(left);
-
-    var right = document.createElement("a-entity");
-    right.setAttribute('geometry', 'primitive: plane; width:1; height:1;');
-    right.setAttribute('shadow', 'receive: true');
-    right.setAttribute('material', 'shader:flat; opacity:0.3; color: white');
-    right.setAttribute('position', '1 0 0.01');
-    // right.el.getOrCreateObject3D('mesh').material = new THREE.ShadowMaterial({
-    //   // ...
-    // });
-    // right.material.opacity = 0.3;
-    this.el.appendChild(right);
-
-    var bottom = document.createElement("a-entity");
-    bottom.setAttribute('geometry', 'primitive: plane; width:1; height:1;');
-    bottom.setAttribute('shadow', 'receive: true');
-    bottom.setAttribute('material', 'shader:flat; opacity:0.3; color: white');
-    bottom.setAttribute('position', '0 -1 0.01');
-    // bottom.el.getOrCreateObject3D('mesh').material = new THREE.ShadowMaterial({
-    //   // ...
-    // });
-    // bottom.material.opacity = 0.3;
-    this.el.appendChild(bottom);
-
-
-    this.el.setAttribute('geometry', 'primitive: plane; width:0.1; height:0.1;');
-    this.el.setAttribute('position', '0 0.5 0.01');
-    // this.el.setAttribute('shadow', 'receive: true');
-
-    this.material = this.el.getOrCreateObject3D('mesh').material = new THREE.ShadowMaterial({
-      // ...
-    });
-    this.material.opacity = 0.3;
-
-  },
-
-  update: function () {
-    // Update `this.material`.
-  }
+    }
 });
-
 
 //Optical crosshair (Custom Cursor Element).
 AFRAME.registerComponent("crosshair", {
@@ -144,27 +104,27 @@ AFRAME.registerComponent("crosshair", {
         //Base ring foreground.
         var _01 = document.createElement("a-entity");
         _01.setAttribute('geometry', 'primitive: ring; radiusInner: 0.018; radiusOuter: 0.02;');
-        _01.setAttribute('material', 'shader: flat; color: #EC8F2D');
+        _01.setAttribute('material', 'shader: flat; color: #EE5C4E');
         this.el.appendChild(_01);
 
         var _a2 = document.createElement("a-entity");
         _a2.setAttribute('geometry', 'primitive: ring; radiusInner: 0.01; radiusOuter: 0.02; thetaStart:43; thetaLength:4;');
-        _a2.setAttribute('material', 'shader: flat; color: #EC8F2D');
+        _a2.setAttribute('material', 'shader: flat; color: #EE5C4E');
         this.el.appendChild(_a2);
 
         var _b2 = document.createElement("a-entity");
         _b2.setAttribute('geometry', 'primitive: ring; radiusInner: 0.01; radiusOuter: 0.02; thetaStart:133; thetaLength:4;');
-        _b2.setAttribute('material', 'shader: flat; color: #EC8F2D');
+        _b2.setAttribute('material', 'shader: flat; color: #EE5C4E');
         this.el.appendChild(_b2);
 
         var _c2 = document.createElement("a-entity");
         _c2.setAttribute('geometry', 'primitive: ring; radiusInner: 0.01; radiusOuter: 0.02; thetaStart:223; thetaLength:4;');
-        _c2.setAttribute('material', 'shader: flat; color: #EC8F2D');
+        _c2.setAttribute('material', 'shader: flat; color: #EE5C4E');
         this.el.appendChild(_c2);
 
         var _d2 = document.createElement("a-entity");
         _d2.setAttribute('geometry', 'primitive: ring; radiusInner: 0.01; radiusOuter: 0.02; thetaStart:313; thetaLength:4;');
-        _d2.setAttribute('material', 'shader: flat; color: #EC8F2D');
+        _d2.setAttribute('material', 'shader: flat; color: #EE5C4E');
         this.el.appendChild(_d2);
     }
 });
