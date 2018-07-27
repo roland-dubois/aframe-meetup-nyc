88172bf7c7368cd31c22e923705358d2e25d2ad9
rdub80 <rdub80@gmail.com>
2018-07-27 11:16:25 -0400

    catch error script

--- demos/00_A-Frame_starter/app/js/main.js
+++ demos/00_A-Frame_starter/app/js/main.js
@@ -1,3 +1,8 @@
+/* global AFRAME, THREE */
+if (typeof AFRAME === 'undefined') {
+  throw new Error('Component attempted to register before AFRAME was available.');
+}
+
 AFRAME.registerComponent('custom-component', {
   init: function () {
     var targetEl = this.el;    
--- demos/09_A-Frame_NYC_Bowling/app/js/main.js
+++ demos/09_A-Frame_NYC_Bowling/app/js/main.js
@@ -1,3 +1,8 @@
+/* global AFRAME, THREE */
+if (typeof AFRAME === 'undefined') {
+  throw new Error('Component attempted to register before AFRAME was available.');
+}
+
 AFRAME.registerComponent('launchpad', {
     dependencies: ['raycaster'],
 
