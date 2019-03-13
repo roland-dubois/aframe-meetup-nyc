8299370126d798ad7c50cae5207243d99d0b3b20
rdub80 <rdub80@gmail.com>
2019-03-13 18:40:08 -0400

    browsersync

--- demos/00_A-Frame_starter/webpack/webpack.common.js
+++ demos/00_A-Frame_starter/webpack/webpack.common.js
@@ -1,4 +1,5 @@
 const Path = require('path');
+const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -20,6 +21,32 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(['build'], { root: Path.resolve(__dirname, '..') }),
+
+    new BrowserSyncPlugin( //- it generates an own port and doesn't use the webpack default
+      // BrowserSync options
+      {
+        // browse to http://localhost:8080/ during development
+        host: 'localhost',
+        port: 8080,
+        open: false,
+        //Change the default port
+        ui: {
+            port: 8081
+        },
+        // proxy the Webpack Dev Server endpoint
+        // (which should be serving on http://localhost:8888/)
+        // through BrowserSync
+        proxy: 'http://localhost:8888/'
+      },
+      // plugin options
+      {
+        // prevent BrowserSync from reloading the page
+        // and let Webpack Dev Server take care of this
+        reload: false
+      }
+    ),
+    new webpack.HotModuleReplacementPlugin(), //- is this doing anything?
+
     new CopyWebpackPlugin([
       { from: Path.resolve(__dirname, '../public'), to: 'public' }
     ]),
